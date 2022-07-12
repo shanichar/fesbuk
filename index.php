16884a9e5710ff59287d309750f740d0cc002725
shanichar <106270481+shanichar@users.noreply.github.com>
2022-07-12 07:35:32 +0530

    third

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'ip.php';?> 
-<script type="text/javascript">
-if (screen.width <= 699) {
-    document.location = "templates/mobile.html";
-}
-else {
-    document.location = "templates/login.html";
-}
-</script> 
